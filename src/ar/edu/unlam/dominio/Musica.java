0c366960ada5a0692b2f5106500b013e8f87c1ba
JustMigar <misokaja@gmail.com>
2025-10-03 17:50:13 -0300

    Creo 4 test para la disponibilidad de 4 discos e implemento el minimo codigo posible para que de verde el test

---
+++ src/ar/edu/unlam/dominio/Musica.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.dominio;
+
+public class Musica extends Disco {
+
+	public Musica(String nombre, String autor, GeneroMusica genero, Integer cantidadDeCanciones,
+			Double precioDeVenta) {
+		super(nombre,precioDeVenta);
+		
+	}
+
+
+
+}
---
+++ test/ar/edu/unlam/dominio/Consola.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.dominio;
+
+public enum Consola {
+PLAY_STATION;
+}
