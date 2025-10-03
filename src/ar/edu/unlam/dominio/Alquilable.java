84057ba3f771423dc086623444cc15498dffd0a1
micafurlan <mariaan7448@gmail.com>
2025-10-03 19:53:08 -0300

    lo mismo

--- src/ar/edu/unlam/dominio/Alquilable.java
+++ src/ar/edu/unlam/dominio/Alquilable.java
@@ -1,5 +1,9 @@
 package ar.edu.unlam.dominio;
 
 public interface Alquilable {
-
+	Double precioAlquiler();
+	Boolean alquilarProducto();
+	void devolverProducto(); // si se devuelve y esta vencido que le devuelve al usuario?
+	Double calcularPrecio();
+	Double calcularPrecioAlquilerVencido();
 }
--- src/ar/edu/unlam/dominio/Alquiler.java
+++ src/ar/edu/unlam/dominio/Alquiler.java
@@ -6,17 +6,25 @@ public class Alquiler extends Operacion {
 	private LocalDateTime fechaVencimiento;
 	private LocalDateTime fechaDevolucion;
 	private Double recargo;
-	private Boolean alquilado;
 
-	public Alquiler(Cliente cliente, Disco disco, LocalDateTime fechaEmision, Double precio) {
-		super(cliente, disco, fechaEmision, precio);
-		this.alquilado = true;
+	public Alquiler(Cliente cliente, Disco disco, LocalDateTime fechaEmision) {
+		super(cliente, disco, fechaEmision);
 
 	}
-	public Boolean alquilar(Cliente cliente,Disco disco, LocalDateTime fechaEmision,Double precio) {
-		
+
+	public Boolean alquilarProducto(Cliente cliente, Disco disco, LocalDateTime fechaEmision, Double precio) {
+		if (disco.getEstaDisponible && disco instanceof Alquilable) {
+			if (cliente instanceof ClienteNormal) {
+				this.fechaVencimiento = getFechaEmision().plusDays(5);
+			} else {
+				this.fechaVencimiento = getFechaEmision().plusDays(7);
+			}
+			return true;
+		}
+		return false;
 	}
-	public Boolean devolucion() {
+
+	public Double devolucion(Cliente cliente, Disco disco, LocalDateTime fechaDevolucion) {
 		
 	}
 }
