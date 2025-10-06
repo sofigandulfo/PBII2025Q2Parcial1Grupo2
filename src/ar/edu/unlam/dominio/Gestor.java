02e62444bff444b73e5c572b29be6e20d961eb89
sofigandulfo <sofigandulfo@gmail.com>
2025-10-05 21:28:22 -0300

    agrego mas test, implemeto metodo en cliente para los strikes y sumo modificaciones en algunos archivos

--- src/ar/edu/unlam/dominio/Gestor.java
+++ src/ar/edu/unlam/dominio/Gestor.java
@@ -43,6 +43,13 @@ public class Gestor {
 		return false;
 	}
 
+	public Double obtenerPrecioAlquiler(Operacion operacion) {
+		if(operacion instanceof Alquiler) {
+			Alquiler alquiler = (Alquiler)operacion;
+			return alquiler.calcularPrecioFinal();
+		}
+		return null;
+	}
 
 
 
@@ -59,7 +66,7 @@ public class Gestor {
 	}
 
 
-	public Alquiler encontrarDiscoAlquilado(Disco disco, Cliente cliente) {
+	public Alquiler encontrarAlquilerDelDisco(Disco disco, Cliente cliente) {
 		for(Operacion operacion : operaciones) {
 			if(operacion instanceof Alquiler) {
 				Alquiler alquiler = (Alquiler) operacion;
@@ -71,10 +78,9 @@ public class Gestor {
 		return null;
 	}
 
-	// chicos les aviso q esta es la base del metodo nomas 
 	public Boolean devolverDisco(Disco disco, Cliente cliente, LocalDateTime fechaDevolucion) {
-		Alquiler alquiler = encontrarDiscoAlquilado(disco, cliente);
-		if(alquiler != null && !fechaDevolucion.isBefore(alquiler.getFechaEmision())) { //no se puede devolver si la fecha de devolucion es anterior a la fecha de emision del alquiler
+		Alquiler alquiler = encontrarAlquilerDelDisco(disco, cliente);
+		if(alquiler != null && !fechaDevolucion.isBefore(alquiler.getFechaEmision())) { 
 			alquiler.devolverDisco(fechaDevolucion);
 			return true;
 		}
@@ -99,6 +105,15 @@ public class Gestor {
 			return ((Venta)venta).obtenerPrecioFinal();
 		}
 		
+		return null;
+	}
+	
+	public Operacion obtenerOperacion(Disco disco, Cliente cliente) {
+		for(Operacion operacion : operaciones) {
+			if(operacion.getDisco().equals(disco) && operacion.getCliente().equals(cliente)) {
+				return operacion;
+			}
+		}
 		return null;
 	}
 	
--- src/ar/edu/unlam/dominio/Pelicula.java
+++ src/ar/edu/unlam/dominio/Pelicula.java
@@ -1,12 +1,23 @@
 package ar.edu.unlam.dominio;
 
+
 public class Pelicula extends Disco implements Alquilable{
+
+	private GeneroPelicula genero;
+	private Integer anio;
+	private String director;
+	private Integer duracion;
 	private Double precioAlquiler;
+	
 	public Pelicula(String nombre, GeneroPelicula genero, Integer anio, String director, Integer duracion,
 			Double precioVenta, Double precioAlquiler) {
 		super(nombre,precioVenta);
-		
-		this.precioAlquiler=precioAlquiler;
+		this.genero = genero;
+		this.anio = anio;
+		this.director = director;
+		this.duracion = duracion;
+		this.precioAlquiler = precioAlquiler;
+
 	}
 	public String getTipo(){
 		return "Pelicula";
@@ -18,5 +29,25 @@ public class Pelicula extends Disco implements Alquilable{
 	
 	public Double obtenerPrecioAlquilerPorDiaAtrasado() {
 		return this.precioAlquiler*1.5;
+	}
+	@Override
+	public Boolean alquilarProducto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void devolverProducto() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Double calcularPrecio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Double calcularPrecioAlquilerVencido() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
